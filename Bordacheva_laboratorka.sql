0d3948f4d9479b23829feabbff8165ca132a4890
Юлия Бордачева <s_proza@list.ru>
2019-02-25 17:08:38 +0300

    'diplom2'

--- Bordacheva_laboratorka.sql
+++ Bordacheva_laboratorka.sql
@@ -1,3 +1,6 @@
+
+---1 запрос Вывести список названий департаментов и количество главных врачей в каждом из этих департаментов
+
 (SELECT
    department.name,
    COUNT (DISTINCT chief_doc_id) as chief_doc_count
@@ -8,6 +11,8 @@ WHERE
  GROUP BY department.name);
 
 
+
+---2 запрос Вывести список департаментов, в которых работают 3 и более сотрудников (id и название департамента, количество сотрудников)
  (
    SELECT
       department.id,
@@ -16,51 +21,54 @@ WHERE
    FROM department
      LEFT JOIN employee
      ON department.id = employee.department_id
-     GROUP BY department.id
+     GROUP BY department.id, department.name
      HAVING COUNT (employee.id) >=3
    );
 
 
+----3 запрос Вывести список департаментов с максимальным количеством публикаций  (id и название департамента, количество публикаций)
+
+   WITH department_public AS
 
    (
    SELECT
       department.id,
       department.name,
-      SUM(num_public) as max_public_num
+      SUM(employee.num_public) as total_public_num
    FROM department
-     LEFT JOIN employee
+     JOIN employee
      ON department.id = employee.department_id
-     GROUP BY department.id
-     HAVING  MAX(max_public_num)
-   );
+     GROUP BY department.id, department.name
+     ORDER BY total_public_num DESC
+   )
+SELECT * FROM
+department_public WHERE total_public_num = (
+  SELECT total_public_num FROM department_public LIMIT 1
+     );
+ ----- 4 запрос Вывести список сотрудников с минимальным количеством публикаций в своем департаменте (id и название департамента, имя сотрудника, количество публикаций)
+  WITH mp AS (
+	SELECT
+  		MIN(employee.num_public) AS num_public,
+  		employee.department_id
+	FROM employee
+	GROUP BY employee.department_id
+)
+SELECT department.id, department.name, employee.name, employee.num_public FROM mp
+FULL OUTER JOIN employee ON employee.num_public = mp.num_public
+JOIN department ON department.id = employee.department_id
+WHERE department.id = mp.department_id AND employee.num_public = mp.num_public
+ORDER BY department.id;
 
-   SELECT
-   department.id, department.name, employee.name, MIN(num_public) as min_public
-FROM(
-SELECT department_values.id AS department.id, department_values.name AS department.name, employee_values.name as employee, num_public
-    FROM department_values
-    JOIN employee_values
-        ON department_values.id =employee_values.department_id
-)as new_table
-GROUP BY department.id;
 
+----5 запрос Вывести список департаментов и среднее количество публикаций для тех департаментов, в которых работает более одного главного врача (id и название департамента, среднее количество публикаций)
 
-
-
-WITH
-
-dep_public as (
-department.id, department.name,  SUM (num_public) as Summary
-FROM Department
-    LEFT JOIN Employee
-    ON Department.id=Employee.department_id
-    GROUP BY Department.name, Department.id
-    ORDER BY Summary DESC)
 SELECT
-     Department.id,
-    Department.Name,
-     Summary
-FROM dep_public
-    where
+	department.id,
+	department.name,
+	AVG(employee.num_public) AS avg
+FROM department
+JOIN employee  ON department.id = employee.department_id
+GROUP BY department.id, department.name
+HAVING COUNT(DISTINCT(employee.chief_doc_id)) > 1;
 
 
